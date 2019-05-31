25ebbdc401a080ca70a834a3d8f44f98a14b97b8
Lewis Ugege <31358867+frankly034@users.noreply.github.com>
2019-05-31 13:27:56 +0100

    chore(test): setup test environment (#3)
    - install babel dev-dependencies: @babel/core @babel/node @babel/preset-env @babel/register
    - install test dev-dependencies: mochai, chai, chai-http
    - setup babel
    - create default test folder
    - create default test file
    
    [Delivers #166272095]

---
+++ test/index.spec.js
@@ -0,0 +1,14 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Default test', () => {
+  context('Basic test', () => {
+    it('Should call expect from chai', () => {
+        expect(2).to.equal(2);
+    });
+  });
+});
