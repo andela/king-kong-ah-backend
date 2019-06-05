a86f9299cd658f6750722fa4efb5accf8a5385a9
Lewis Ugege <lewisugege@Lewiss-MacBook-Pro.local>
2019-06-05 11:49:03 +0100

    chore(models):setup user model
    - install bcryptjs
    - add user migration and model
    - add hashPassword function
    - hash password beforeCreate
    - add .editorconfig file
    - add hashPassword utility function
    - remove unused dependencies
    - add tests for user model and hashPassword utility function
    
    [Chore #166411032]

---
+++ server/migrations/user.js
@@ -0,0 +1,54 @@
+const user = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Users', {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true
+    },
+    username: {
+      type: Sequelize.STRING,
+      unique: true
+    },
+    bio: {
+      type: Sequelize.TEXT,
+      allowNull: true
+    },
+    profileImage: {
+      type: Sequelize.TEXT,
+      allowNull: true
+    },
+    lastName: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    firstName: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    isAdmin: {
+      type: Sequelize.BOOLEAN,
+      default: false
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }),
+  down: queryInterface => queryInterface.dropTable('Users')
+};
+
+export default user;
