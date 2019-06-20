60814ac4f07caee91e36b3c3234bf8081c2d8b8a
Oluwatosin, Akinyele <oluwatosin.akinyele@andela.com>
2019-06-20 19:06:30 +0100

    bug(test):fix login agent
    - fix article spec
    - fix profile spec
    - fix tagArticle spec
    - fix helpers/util.js
    
    [Fixes #166813827]

--- server/tests/helpers/utils.js
+++ server/tests/helpers/utils.js
@@ -4,24 +4,19 @@ import { getUserData } from '<fixtures>/user';
 
 const { Category, User, Tag } = models;
 
-export const signupUser = agent => new Promise((resolve) => {
-  agent
-    .post('/api/v1/auth/signup')
-    .send(getUserData({
-      email: 'johndoe@email.com', username: 'johndoee', password: '123456abcdef'
-    }))
-    .then(() => {
-      agent
-        .post('/api/v1/auth/login')
-        .send({
-          email: 'johndoe@email.com',
-          password: '123456abcdef'
-        })
-        .then(() => {
-          resolve();
-        });
-    });
-});
+export const signupUser = agent => agent
+  .post('/api/v1/auth/signup')
+  .send(getUserData({
+    email: 'johndoe@email.com', username: 'johndoee', password: '123456abcdef'
+  }));
+
+export const loginUser = agent => agent
+  .post('/api/v1/auth/login')
+  .send({
+    email: 'johndoe@email.com',
+    password: '123456abcdef'
+  });
+
 
 export const getCategoryId = async (values) => {
   try {
