613f3275803ba8748735625f2e79708b5592ca5d
Akinyele Oluwatosin Akintayo <Akinyeleolat2005@gmail.com>
2019-08-01 14:39:54 +0100

    fix(rating): fix append rating for all (#46)
    - fix article rating for get all article
    - fix article rating for get article by category
    
    [Fixes #166272109]

--- server/helpers/getArticleRating.js
+++ server/helpers/getArticleRating.js
@@ -3,7 +3,7 @@ import models from '<serverModels>';
 const { Rating, sequelize } = models;
 
 
-const getArticleRating = async (articleId) => {
+export const getArticleRating = async (articleId) => {
   const { dataValues } = await Rating.findOne({
     where: { articleId },
     attributes: [[sequelize.fn('AVG', sequelize.col('rating')), 'averageRating']],
@@ -11,4 +11,14 @@ const getArticleRating = async (articleId) => {
   const { averageRating } = dataValues;
   return Math.round(averageRating);
 };
-export default getArticleRating;
+
+export const appendRating = async (articleData) => {
+  const result = await Promise.all(
+    articleData.map(async (article) => {
+      const averageRating = await getArticleRating(article.id);
+      article.dataValues.rating = averageRating;
+      return article.dataValues;
+    })
+  );
+  return result;
+};
